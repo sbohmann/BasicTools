797fdb54741a7b1cf86cfbda61b43fe6013eafe3
sbohmann <mail@sebastian-bohmann.com>
2019-03-12 21:57:41 +0100

    Modernized and sanitized the command line parser test suite

--- test/at/yeoman/tools/clp/CommandLineParserTest.java
+++ test/at/yeoman/tools/clp/CommandLineParserTest.java
@@ -8,104 +8,79 @@ public class CommandLineParserTest
 {
 	@Test
 	public void basicBehavior()
-	throws Exception
+			throws Exception
 	{
 		CommandLineParser parser = new CommandLineParser();
 		Key<String> hostKey = parser.addStringArgument("host");
 		Key<Integer> portKey = parser.addIntArgument("port");
 		Key<Boolean> connectKey = parser.addOption("connect");
-		
+
 		parser.parse(new String[] { "-port", "12345", "-host", "localhost" });
-		
+
 		String host = parser.get(hostKey);
 		int port = parser.get(portKey);
 		boolean connect = parser.get(connectKey);
-		
+
 		System.out.println(host + ":" + port + ", connect: " + connect);
+
+		Assert.assertEquals("localhost", host);
+		Assert.assertEquals(12345, port);
+		Assert.assertFalse(connect);
 	}
-	
+
 	@Test
 	public void basicBehaviorWithOptionSet()
-	throws Exception
+			throws Exception
 	{
 		CommandLineParser parser = new CommandLineParser();
 		Key<String> hostKey = parser.addStringArgument("host");
 		Key<Integer> portKey = parser.addIntArgument("port");
 		Key<Boolean> connectKey = parser.addOption("connect");
-		
+
 		parser.parse(new String[] { "-connect", "-port", "12345", "-host", "localhost" });
-		
+
 		String host = parser.get(hostKey);
 		int port = parser.get(portKey);
 		boolean connect = parser.get(connectKey);
-		
+
 		System.out.println(host + ":" + port + ", connect: " + connect);
+
+		Assert.assertEquals("localhost", host);
+		Assert.assertEquals(12345, port);
+		Assert.assertTrue(connect);
 	}
-	
-	@Test
+
+	@Test(expected=ParsingException.class)
 	public void basicBehaviorWithOptionDoublySet()
-	throws Exception
+			throws Exception
 	{
 		CommandLineParser parser = new CommandLineParser();
 		parser.addStringArgument("host");
 		parser.addIntArgument("port");
 		parser.addOption("connect");
-		
-		try
-		{
-			parser.parse(new String[] { "-connect", "-port", "12345", "-host", "localhost", "-connect" });
-		}
-		catch(ParsingException exc)
-		{
-			System.err.println(exc.getMessage());
-			
-			return;
-		}
-		
-		Assert.fail();
+
+		parser.parse(new String[] { "-connect", "-port", "12345", "-host", "localhost", "-connect" });
 	}
-	
-	@Test
+
+	@Test(expected=ParsingException.class)
 	public void missingHostArgument()
-	throws Exception
+			throws Exception
 	{
 		CommandLineParser parser = new CommandLineParser();
 		parser.addStringArgument("host");
 		parser.addIntArgument("port");
-		
-		try
-		{
-			parser.parse(new String[] { "-port", "12345" });
-		}
-		catch(ParsingException exc)
-		{
-			System.err.println(exc.getMessage());
-			
-			return;
-		}
-		
-		Assert.fail();
+
+		parser.parse(new String[] { "-port", "12345" });
 	}
-	
-	@Test
+
+	@Test(expected=ParsingException.class)
 	public void missingPortArgument()
-	throws Exception
+			throws Exception
 	{
 		CommandLineParser parser = new CommandLineParser();
 		parser.addStringArgument("host");
 		parser.addIntArgument("port");
-		
-		try
-		{
-			parser.parse(new String[] { "-host", "localhost" });
-		}
-		catch(ParsingException exc)
-		{
-			System.err.println(exc.getMessage());
-			
-			return;
-		}
-		
-		Assert.fail();
+
+		parser.parse(new String[] { "-host", "localhost" });
 	}
 }
